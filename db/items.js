c9662a78833fe35c7c5e232df87f7688365db1f1
Gildardo Garcia <GildardoGarcia@Gildardos-MacBook-Pro.local>
2019-06-13 12:37:24 -0700

    commit

--- db/items.js
+++ db/items.js
@@ -2,6 +2,7 @@ const name = 'items'
 
 let schema = {
     id: 'number',
+    itemNumber: 'number',
     name: 'string',
     description: 'string',
     lotId: 'number'
@@ -11,6 +12,7 @@ let schema = {
 let data = [
     {
         id: 1,
+        itemNumber: 378,
         name: "Engine Oil",
         description: "Valvoline 10W-30",
         lotId: 100
@@ -18,6 +20,7 @@ let data = [
 
     {
         id: 2,
+        itemNumber: 698,
         name: "Engine Oil",
         description: "Castrol 10W-30",
         lotId: 100
@@ -25,6 +28,7 @@ let data = [
 
     {
         id: 3,
+        itemNumber: 132,
         name: "Oil Filter",
         description: "FRAM PH-7248",
         lotId: 103
@@ -32,6 +36,7 @@ let data = [
 
     {
         id: 4,
+        itemNumber: 156,
         name: "Air Filter",
         description: "FRAM CA11113",
         lotId: 101
@@ -39,6 +44,7 @@ let data = [
 
     {
         id: 5,
+        itemNumber: 179,
         name: "Air Filter",
         description: "K&N 33-2468",
         lotId: 101
@@ -46,6 +52,7 @@ let data = [
 
     {
         id: 6,
+        itemNumber: 1045,
         name: "Beam Electronics",
         description: "Universal Smartphone Car Air Vent Mount Holder",
         lotId: 101
@@ -53,6 +60,7 @@ let data = [
 
     {
         id: 7,
+        itemNumber: 857,
         name: "Car Spares",
         description: "Firestone Tires 205/55R16",
         lotId: 300
--- db/lots.js
+++ db/lots.js
@@ -2,6 +2,7 @@ const name = 'lots'
 
 let schema = {
     id: 'number',
+    lotNumber: 'number',
     title: 'string',
     description: 'string',
     location: 'string'
@@ -10,42 +11,48 @@ let schema = {
 // contenido inicial
 let data = [
     {
-        id: 100,
+        id: 1,
+        lotNumber: 100,
         title: "Car Maintance",
         description: "Engine Oil",
         location: "Zone A-6"
     },
 
     {
-        id: 101,
+        id: 2,
+        lotNumber: 101,
         title: "Car Maintance",
         description: "Air Filter",
         location: "Zone A-2"
     },
 
     {
-        id: 103,
+        id: 3,
+        lotNumber: 103,
         title: "Car Maintance",
         description: "Oil Filter",
         location: "Zone A-1"
     },
 
     {
-        id: 200,
+        id: 4,
+        lotNumber: 200,
         title: "Car Accesories",
-        description: "Cellphone Mount",
+        description: "Cellphone",
         location: "Zone B-1"
     },
 
     {
-        id: 300,
+        id: 5,
+        lotNumber: 300,
         title: "Car Spares",
         Description: "Tires",
         location: "Zone F-3"
     },
 
     {
-        id: 301,
+        id: 6,
+        lotNumber: 301,
         title: "Car Spares",
         Description: "Radiator Hoses",
         location: "Zone C-9"
